9dbd6a7ed33772db0867b6dac5be62eadf3e76e5
DylanChi <dylanchi@cw.com.tw>
2020-11-17 19:05:46 +0800

    add centos8 and php74

---
+++ docker/docker-images/php-fpm-7.4-base/image.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+PHP_VERSION="7.4"
+IMAGE_NAME="whcdc/php-fpm:${PHP_VERSION}"
+
+case "$1" in
+    build)
+        docker build -t $IMAGE_NAME .
+        ;;
+
+    push)
+        docker push $IMAGE_NAME
+        ;;
+    *)
+        echo $"Usage: $0 {build|push}"
+        exit 1
+esac
