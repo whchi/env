472b0e33487e7cd4912e4cef7e26fafa8d5bb704
dylanchi <dylan_chi@owlting.com>
2021-07-01 17:18:48 +0800

    add data

---
+++ scripts/install_dev_ubuntu20.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+sudo apt update
+
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
+
+# docker
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+
+sudo apt update
+
+sudo apt install -y docker-ce
+
+# docker-compose
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+sudo chmod +x /usr/local/bin/docker-compose
+
+# git
+
+sudo apt install -y git
